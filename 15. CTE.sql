1bbc7b671bdfea5094e039bb4f580803b1eefd6f
jeeva jothiga <123095643+Jeevajothiga@users.noreply.github.com>
2025-01-03 21:05:17 +0530

    Update 15. CTE.sql

--- 15. CTE.sql
+++ 15. CTE.sql
@@ -7,7 +7,7 @@
 
 
 -- First, CTEs start using a "With" Keyword. Now we get to name this CTE anything we want
--- Then we say as and within the parenthesis we build our subquery/table we want
+-- Then we say as and within the parenthesis we build our subquery/table we want.    
 WITH CTE_Example AS 
 (
 SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary), AVG(salary)
